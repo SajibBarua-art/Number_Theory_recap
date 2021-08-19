770ba63cd9a97ae61cdc9769940a3f9d272a1bbe
SajibBarua-art <sajibbaruanstu@gmail.com>
2021-08-19 16:01:25 +0600

    day-3

---
+++ Farmat primilaty test.cpp
@@ -0,0 +1,60 @@
+// It's can determine n is prime or not at n = 2^64-1 or 10^18 maximum limitation
+// But it's accuracy is not always 100%
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+ll mulmod(ll a, ll b,ll c){
+    ll x = 0,y=a%c;
+    while(b > 0){
+		if(b%2 == 1){
+			x = (x+y)%c;
+		}
+		y = (y*2LL)%c;
+		b /= 2;
+    }
+    return x%c;
+}
+
+ll binPower(ll a , ll n , ll mod)
+{
+	ll res = 1;
+	while(n)
+	{
+		if(n & 1)
+		res = mulmod(res , a , mod);
+		
+		n >>= 1;
+		a = mulmod(a , a , mod);
+	}
+	
+	return res % mod;
+}
+
+bool isPrime(ll n , int iterations = 10)
+{
+	if(n <= 4)
+	return n == 2 || n == 3;
+	
+	for(int i=1;i<=iterations;i++)
+	{
+		ll a = 2 + rand() % (n - 3);
+		ll res = binPower(a , n - 1 , n);
+		
+		if(res != 1) return false;
+	}
+	return true;
+}
+
+int main()
+{
+	ll t , n;
+	cin>>t;
+	while(t--)
+	{
+		cin>>n;
+		if(isPrime(n)) cout<<"YES"<<endl;
+		else		   cout<<"NO"<<endl;
+	}
+}
---
+++ Miller Rabin primilaty test.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+using u64 = uint64_t;
+using u128 = __uint128_t;
+ 
+u64 binpower(u64 base, u64 e, u64 mod) {
+    u64 result = 1;
+    base %= mod;
+    while (e) {
+        if (e & 1)
+            result = (u128)result * base % mod;
+        base = (u128)base * base % mod;
+        e >>= 1;
+    }
+    return result;
+}
+ 
+bool check_composite(u64 n, u64 a, u64 d, int s) {
+    u64 x = binpower(a, d, n);
+    if (x == 1 || x == n - 1)
+        return false;
+    for (int r = 1; r < s; r++) {
+        x = (u128)x * x % n;
+        if (x == n - 1)
+            return false;
+    }
+    return true;
+};
+ 
+bool MillerRabin(u64 n) {
+    if (n < 4)
+        return n == 2 || n == 3;
+ 
+    int s = 0;
+    u64 d = n - 1;
+    while ((d & 1) == 0) {
+        d >>= 1;
+        s++;
+    }
+ 
+    for (int a: {2, 3, 5, 7, 11, 13, 15, 17, 19, 23, 29, 31, 37}) {
+        if (check_composite(n, a, d, s))
+            return false;
+    }
+    return true;
+}
+ 
+int main ()
+{
+	u64 t, n; cin >> t;
+	while(t--)
+	{
+		cin >> n;
+		if(MillerRabin(n)) cout << "YES\n";
+		else cout << "NO\n";
+	}	
+}
